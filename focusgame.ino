d6030345d07364ae0e6780fbf3dc06d346d0cc29
NartRamadan <kataz91@gmail.com>
2024-08-13 12:20:40 +0300

    Add files via upload
    testing

---
+++ focusgame.ino
@@ -0,0 +1,11 @@
+#include "pitches.h"
+
+#define LED_RED 2
+#define LED_YELLOW 3
+#define LED_GREEN 4
+#define LED_BLUE 12
+
+#define BTN_RED 6
+#define BTN_YELLOW 9
+#define BTN_GREEN 10
+#define BTN_BLUE 11
