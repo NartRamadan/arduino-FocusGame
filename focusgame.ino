8049d232cc90a7f54b576f4a105f21b5cfc5bbc4
NartRamadan <kataz91@gmail.com>
2024-08-13 12:45:56 +0300

    עדכון ראשוני?

--- focusgame.ino
+++ focusgame.ino
@@ -1,11 +1,18 @@
-#include "pitches.h"
+#include "pitches.h"  // כוללת את ספריית הצלילים
 
+// הגדרת פינים ללדים
 #define LED_RED 2
 #define LED_YELLOW 3
 #define LED_GREEN 4
 #define LED_BLUE 12
 
+// הגדרת כפתורים
 #define BTN_RED 6
 #define BTN_YELLOW 9
 #define BTN_GREEN 10
 #define BTN_BLUE 11
+
+// הגדרת מספר לדים ומקסימום לדים במשחק
+#define NUM_COLORS 4
+#define SEQ_LENGTH 3
+#define BUZZER_PIN 5
